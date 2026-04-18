4b03b4529b95f70d4b43d58b6207cc4916e7d31a
Gagan T S <gagants2006@gmail.com>
2026-04-18 11:15:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("🖼️ Text to Image Generator")
 
 @st.cache_resource
 def load_model():
-    model_id = "baidu/ERNIE-Image"
+    model_id = "unsloth/ERNIE-Image-Turbo-GGUF"
 
     pipe = StableDiffusionPipeline.from_pretrained(
         model_id,
